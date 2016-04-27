e6ca2a8a80fd63cc9373296c7785b185e16eb2e5
SuperCoderX <xyb930826@gmail.com>
2016-04-27 22:28:33 +0800

    modify animation, use canvas

--- chapter 10/prepareSlideshow.js
+++ chapter 10/prepareSlideshow.js
@@ -2,19 +2,35 @@ function prepareSlideshow () {
 	if (!document.getElementsByTagName) return false;
 	if(!document.getElementById) return false;
 	if(!document.getElementById('linklist')) return false;
-	if(!document.getElementById('slideshow')) return false;
+	//if(!document.getElementById('slideshow')) return false;
+
+	//创建元素
+	var slideshow = document.createElement('div');
+	slideshow.setAttribute('id', 'slideshow');
 
 	//为图片应用样式
-	var preview = document.getElementById('preview');
+	var preview = document.createElement('img');//document.getElementById('preview');
+	preview.setAttribute('src', 'topics.gif');
+	preview.setAttribute('alt','building blocks of web design');
+	preview.setAttribute('id', 'preview');
+	preview.style.top = '0px';
+	preview.style.left = '0px';
+	slideshow.appendChild(preview);
+	
+	//slideshow.style.position = 'absolute';
+	/*
 	preview.style.position = 'absolute';
 	preview.style.top = '0px';
 	preview.style.left = '0px';
-
+	*/
 	//取得链接并添加动画效果
 	var list = document.getElementById('linklist');
+	insertAfter(slideshow, list);
+
 	var links = list.getElementsByTagName('a');
 	links[0].onmouseover = function(){
 		moveElement('preview', -100, 0, 10);
+
 	}
 	links[1].onmouseover = function(){
 		moveElement('preview', -200, 0, 10);
@@ -37,7 +53,7 @@ function moveElement(elementID, final_x, final_y, interval){
 
 	var xPos = parseInt(elem.style.left);
 	var yPos = parseInt(elem.style.top);
-
+	//alert(elem.style.left + "//" + yPos);
 	var dist = 0;
 	var scale = 10;
 
@@ -79,4 +95,13 @@ function addLoadEvent(func){
 			func();
 		}
 	}
+}
+
+function insertAfter(newElement, targetElement){
+	var parent = targetElement.parentNode;
+	if (parent.lastChild == targetElement) {
+		parent.appendChild(newElement);
+	} else{
+		parent.insertBefore(newElement, targetElement.nextSibling);
+	};
 }
--- chapter 11/useCanvas.js
+++ chapter 11/useCanvas.js
@@ -1,3 +1,10 @@
 function draw () {
-	
+	var canvas = document.getElementById('draw-in-me');
+	if (canvas.getContext) {
+		var ctx = canvas.getContext('2d');
+		ctx.beginPath();
+		ctx.moveTo(120.0, 32.0);
+		ctx.closePath();
+		ctx.fill();
+	};
 }
