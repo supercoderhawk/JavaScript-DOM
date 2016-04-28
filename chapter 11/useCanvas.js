4983c9ebf5b1328ae1cc071a5437e535b9145be6
SuperCoderX <xyb930826@gmail.com>
2016-04-28 13:25:06 +0800

    draw a button with canvas

--- chapter 11/useCanvas.js
+++ chapter 11/useCanvas.js
@@ -4,7 +4,34 @@ function draw () {
 		var ctx = canvas.getContext('2d');
 		ctx.beginPath();
 		ctx.moveTo(120.0, 32.0);
+		ctx.bezierCurveTo(120.0, 36.4, 116.4, 40.0, 112.0, 40.0);
+		ctx.lineTo(8.0, 40.0);
+		ctx.bezierCurveTo(3.6, 40.0, 0.0, 36.4, 0.0,32.0);
+		ctx.lineTo(0.0, 8.0);
+		ctx.bezierCurveTo(0.0, 3.6, 3.6, 0.0, 8.0, 0.0);
+		ctx.lineTo(112.0, 0.0);
+		ctx.bezierCurveTo(116.4, 0.0, 120.0, 3.6, 120.0, 8.0);
+		ctx.lineTo(120.0, 32.0);
+
 		ctx.closePath();
 		ctx.fill();
+		ctx.lineWidth = 2.0;
+		ctx.strokeStyle = "rgb(15,255,2)";
+		ctx.stroke();
+
 	};
+}
+
+addLoadEvent(draw);
+
+function addLoadEvent(func){
+	var oldonload = window.onload;
+	if(typeof window.onload != 'function'){
+		window.onload = func;
+	}else{
+		window.onload = function(){
+			oldonload();
+			func();
+		}
+	}
 }
