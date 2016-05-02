e93612b041b1c24940b7008094b19e5bb19e22c8
SuperCoderX <xyb930826@gmail.com>
2016-05-02 20:16:04 +0800

    add slideshow effect

--- chapter 12/scripts/global.js
+++ chapter 12/scripts/global.js
@@ -1,3 +1,94 @@
+function prepareSlideshow(){
+  if(!document.getElementsByTagName) return false;
+  if(!document.getElementById) return false;
+  if(!document.getElementById('intro')) return false;
+
+  //创建幻灯片元素
+  var intro = document.getElementById('intro');
+  var slideShow = document.createElement('div');
+  slideShow.setAttribute('id', 'slideshow');
+  var preview = document.createElement('img');
+  preview.setAttribute('src', 'images/slideshow.gif');
+  preview.setAttribute('alt', 'a glimpse of what awaits you');
+  preview.setAttribute('id', 'preview');
+  //preview.style.position = 'absolute';
+  preview.style.top = '0px';
+  preview.style.left = '0px';
+  slideShow.appendChild(preview);
+  insertAfter(slideShow,intro);
+  
+  //设置幻灯片动画效果
+  var links = intro.getElementsByTagName('a');
+  var destination = null;
+  for (var i = 0; i < links.length; i++) {
+    links[i].onmouseover = function(){
+      destination = this.getAttribute('href');
+      if(destination.indexOf('index.html') != -1){
+
+        moveElement('preview',0,0,5);
+      }
+      if (destination.indexOf('about.html') != -1) {
+        moveElement('preview', -150, 0, 5);
+      }
+      if(destination.indexOf('photos.html') != -1){
+        moveElement('preview', -300, 0, 5);
+      }
+      if(destination.indexOf('live.html') != -1){
+        moveElement('preview', -450, 0, 5);
+      }
+      if(destination.indexOf('contact.html') != -1){
+
+        moveElement('preview', -600, 0, 5);
+      }
+    }
+  };
+}
+
+addLoadEvent(prepareSlideshow);
+
+function moveElement(elementID, final_x, final_y, interval){
+  
+  if (!document.getElementById) return false;
+  if (!document.getElementById(elementID)) return false;
+  var elem = document.getElementById(elementID);
+  if(elem.movementElement){
+    clearTimeout(elem.movementElement);
+  }
+
+  //elem.style.position = 'absolute';
+  var xPos = parseInt(elem.style.left);
+  var yPos = parseInt(elem.style.top);
+  //alert(xPos + "//" + yPos);
+  var dist = 0;
+  var scale = 10;
+
+  if (xPos == final_x && yPos == final_y) {
+    return true;
+  };
+  if (xPos < final_x) {
+    dist = Math.ceil((final_x - xPos)/scale);
+    xPos += dist;
+  }
+  if (xPos > final_x) {
+    dist = Math.ceil((xPos - final_x)/scale);
+    xPos -= dist;
+  }
+  if (yPos < final_y) {
+    dist = Math.ceil((final_y - yPos)/scale);
+    yPos += dist;
+  }
+  if (yPos > final_y) {
+    dist = Math.ceil((yPos - final_y)/scale);
+    yPos -= dist;
+  }
+  elem.style.top = yPos + "px";
+  elem.style.left = xPos + "px";
+
+  var repeat = "moveElement('" + elementID +"'," + final_x + 
+      "," + final_y + "," + interval + ")";
+  elem.movementElement = setTimeout(repeat, interval);
+}
+
 //将函数添加到onload函数
 function addLoadEvent(func){
   var oldOnload = window.onload;
